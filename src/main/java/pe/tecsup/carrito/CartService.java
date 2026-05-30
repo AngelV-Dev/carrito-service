5eec3153d61ba0a9fab8e1fe52118a1d3c6810ba
AngelV-Dev <angelv.27.25@gmail.com>
2026-05-30 10:04:08 -0500

    feat: create base classes and test structure

---
+++ src/main/java/pe/tecsup/carrito/CartService.java
@@ -0,0 +1,6 @@
+package pe.tecsup.carrito;
+
+public class CartService {
+    // No escribas nada aquí todavía.
+    // El código vendrá después de las pruebas.
+}
---
+++ src/test/java/pe/tecsup/carrito/CartServiceTest.java
@@ -0,0 +1,9 @@
+package pe.tecsup.carrito;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("CartService - reglas de negocio del carrito")
+class CartServiceTest {
+    // Aquí escribirás las pruebas
+}
